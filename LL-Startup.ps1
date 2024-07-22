dde89cc1814dcce78089175305f063903370a4d0
WoodneyUK <55578109+WoodneyUK@users.noreply.github.com>
2024-07-22 17:16:49 +0100

    Update LL-Startup.ps1

--- LL-Startup.ps1
+++ LL-Startup.ps1
@@ -6,21 +6,16 @@ Write-Host "Running from Github"
 #pause
 
 ## Approved Device Checks
-
 $ApprovedDevices = Get-Content "X:\OSDCloud\Config\Scripts\Startup\approveddevices.txt"
-
 $model=Get-CimInstance -ClassName Win32_ComputerSystem
 $computermodel=$model.Model.substring(0,4)
-
 If (($model.Manufacturer -eq "Lenovo") -and ($approveddevices -notcontains $computermodel)) { Write-Warning "This Lenovo Device is Not Approved, please exit" }
 
 
 ## Linklaters Office UI
-start-process "X:\OSDCloud\Config\Scripts\Startup\3.0.3.0\x64\UI++64.exe" -argumentlist "/config:X:\OSDCloud\Config\Scripts\Startup\3.0.3.0\UI++.xml" -Wait
-
-$LLOffice = ((get-itemproperty -Path HKLM:Software\Linklaters -Name LLOffice).LLOffice)
-
-Write-Host "Starting Windows Install, and adding Language Pack:$($LLOffice)"
+#start-process "X:\OSDCloud\Config\Scripts\Startup\3.0.3.0\x64\UI++64.exe" -argumentlist "/config:X:\OSDCloud\Config\Scripts\Startup\3.0.3.0\UI++.xml" -Wait
+#$LLOffice = ((get-itemproperty -Path HKLM:Software\Linklaters -Name LLOffice).LLOffice)
+#Write-Host "Starting Windows Install, and adding Language Pack:$($LLOffice)"
 
 #Start-OSDCloud -OSName "Windows 11 23H2 x64" -OSLanguage en-US -OSEdition Enterprise -OSActivation Volume -UpdateWindows
 Start-OSDCloud -findimagefile -ZTI -OSImageIndex 3
@@ -30,8 +25,10 @@ Start-OSDCloud -findimagefile -ZTI -OSImageIndex 3
 New-Item c:\Windows\system32\Linklaters\OOBE -force -ItemType Directory
 #copy-item -path "X:\OSDCloud\Config\OOBEDeploy\OOBEDeploy.ps1" -destination "c:\Windows\system32\Linklaters\OOBE\OOBEDeploy.ps1"
 #Invoke-restmethod https://raw.githubusercontent.com/WoodneyUK/OOBE/main/OOBEDeploy.ps1 | out-file "c:\Windows\system32\Linklaters\OOBE\OOBEDeploy.ps1" -force
-Invoke-restmethod https://raw.githubusercontent.com/WoodneyUK/OOBE/main/OOBEDeploy.ps1 | out-file "c:\osdcloud\scripts\setupcomplete.ps1" -force
-Invoke-restmethod https://raw.githubusercontent.com/WoodneyUK/OOBE/main/setupcomplete.cmd | out-file "c:\osdcloud\scripts\setupcomplete.cmd" -force
+#Invoke-restmethod https://raw.githubusercontent.com/WoodneyUK/OOBE/main/OOBEDeploy.ps1 | out-file "c:\osdcloud\scripts\setupcomplete.ps1" -force
+#Invoke-restmethod https://raw.githubusercontent.com/WoodneyUK/OOBE/main/setupcomplete.cmd | out-file "c:\osdcloud\scripts\setupcomplete.cmd" -force
+Invoke-restmethod https://raw.githubusercontent.com/WoodneyUK/OOBE/main/OOBEDeploy.ps1 | out-file "c:\windows\setup\scripts\oobe.ps1" -force
+Invoke-restmethod https://raw.githubusercontent.com/WoodneyUK/OOBE/main/oobe.cmd | out-file "c:\windows\setup\scripts\oobe.ps1" -force
 
 New-Item c:\windows\panther\unattend -force -ItemType Directory
 copy-item -path "x:\OSDCloud\Config\OOBEDeploy\Unattend.xml" -destination "C:\Windows\panther\unattend\unattend.xml"
