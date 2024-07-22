f3a4167bcd32d0410a3f3d1fef91e5ac2012d369
WoodneyUK <55578109+WoodneyUK@users.noreply.github.com>
2024-07-22 14:28:54 +0100

    Create LL-Startup.ps1

---
+++ LL-Startup.ps1
@@ -0,0 +1,31 @@
+## Approved Device Checks
+
+$ApprovedDevices = Get-Content "X:\OSDCloud\Config\Scripts\Startup\approveddevices.txt"
+
+$model=Get-CimInstance -ClassName Win32_ComputerSystem
+$computermodel=$model.Model.substring(0,4)
+
+If (($model.Manufacturer -eq "Lenovo") -and ($approveddevices -notcontains $computermodel)) { Write-Warning "This Lenovo Device is Not Approved, please exit" }
+
+
+## Linklaters Office UI
+start-process "X:\OSDCloud\Config\Scripts\Startup\3.0.3.0\x64\UI++64.exe" -argumentlist "/config:X:\OSDCloud\Config\Scripts\Startup\3.0.3.0\UI++.xml" -Wait
+
+$LLOffice = ((get-itemproperty -Path HKLM:Software\Linklaters -Name LLOffice).LLOffice)
+
+Write-Host "Starting Windows Install, and adding Language Pack:$($LLOffice)"
+
+#Start-OSDCloud -OSName "Windows 11 23H2 x64" -OSLanguage en-US -OSEdition Enterprise -OSActivation Volume -UpdateWindows
+Start-OSDCloud -findimagefile -ZTI -OSImageIndex 3
+#pause
+
+# Drop a custom unattend.xml which runs a post-install script
+New-Item c:\Windows\system32\Linklaters\OOBE -force -ItemType Directory
+copy-item -path "X:\OSDCloud\Config\OOBEDeploy\OOBEDeploy.ps1" -destination "c:\Windows\system32\Linklaters\OOBE\OOBEDeploy.ps1"
+New-Item c:\windows\panther\unattend -force -ItemType Directory
+copy-item -path "x:\OSDCloud\Config\OOBEDeploy\Unattend.xml" -destination "C:\Windows\panther\unattend\unattend.xml"
+
+Write-Host "Restarting Computer..."
+Restart-Computer -Force
+
+#pause
