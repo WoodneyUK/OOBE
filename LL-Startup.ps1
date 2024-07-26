ca3b048230adf0397126f12c40d40ae6363fe6fd
WoodneyUK <55578109+WoodneyUK@users.noreply.github.com>
2024-07-26 10:04:54 +0100

    Update LL-Startup.ps1

--- LL-Startup.ps1
+++ LL-Startup.ps1
@@ -22,8 +22,8 @@ Write-Host "Starting Windows Install, and adding Language Pack:$($LLOffice)"
 
 start-sleep 5
 
-Start-OSDCloud -OSName "Windows 11 23H2 x64" -OSLanguage en-US -OSEdition Enterprise -OSActivation Volume
-#Start-OSDCloud -findimagefile -ZTI
+#Start-OSDCloud -OSName "Windows 11 23H2 x64" -OSLanguage en-US -OSEdition Enterprise -OSActivation Volume
+Start-OSDCloud -findimagefile -ZTI
 #pause
 
 write-host "Windows Restore complete"
