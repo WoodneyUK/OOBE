f05774e8ca63f3392e87ac710da34d51b1891f8f
WoodneyUK <55578109+WoodneyUK@users.noreply.github.com>
2024-07-26 12:08:58 +0100

    Update setupcomplete.cmd

--- setupcomplete.cmd
+++ setupcomplete.cmd
@@ -1,1 +1,1 @@
-%windir%\system32\windowspowershell\v1.0\powershell.exe -executionpolicy bypass -file c:\osdcloud\scripts\setupcomplete.ps1
+%windir%\system32\windowspowershell\v1.0\powershell.exe -executionpolicy bypass -file c:\osdcloud\scripts\setupcomplete\setupcomplete.ps1
