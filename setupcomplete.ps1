8f7911f87255c4af8013b24e6c86a4b87b52e0a8
WoodneyUK <55578109+WoodneyUK@users.noreply.github.com>
2024-07-29 13:55:03 +0100

    Update setupcomplete.ps1
    Signed-off-by: WoodneyUK <55578109+WoodneyUK@users.noreply.github.com>

--- setupcomplete.ps1
+++ setupcomplete.ps1
@@ -29,11 +29,17 @@ $OOBEScript =@"
 `$Global:Transcript = "`$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-OOBEScripts.log"
 Start-Transcript -Path (Join-Path "`$env:ProgramData\Microsoft\IntuneManagementExtension\Logs\OSD\" `$Global:Transcript) -ErrorAction Ignore | Out-Null
 
-Write-Host -ForegroundColor DarkGray "Installing AutopilotOOBE PS Module"
-Start-Process PowerShell -ArgumentList "-NoL -C Install-Module AutopilotOOBE -Force -Verbose" -Wait
+#Write-Host -ForegroundColor DarkGray "Installing AutopilotOOBE PS Module"
+#Start-Process PowerShell -ArgumentList "-NoL -C Install-Module AutopilotOOBE -Force -Verbose" -Wait
 
 Write-Host -ForegroundColor DarkGray "Installing OSD PS Module"
 Start-Process PowerShell -ArgumentList "-NoL -C Install-Module OSD -Force -Verbose" -Wait
+
+# Install Language Pack
+install-language en-GB
+install-language fr-FR
+
+# Install Feature on Demand
 
 
 # Running Updates
