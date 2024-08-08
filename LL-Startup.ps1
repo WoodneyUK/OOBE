954e7e2bcf7317e0210eef4a03353d401418d4ff
WoodneyUK <55578109+WoodneyUK@users.noreply.github.com>
2024-08-08 11:11:24 +0100

    Update LL-Startup.ps1
    Signed-off-by: WoodneyUK <55578109+WoodneyUK@users.noreply.github.com>

--- LL-Startup.ps1
+++ LL-Startup.ps1
@@ -79,7 +79,7 @@ reg unload "HKLM\NewOS"
 
 # Apply Latest CU
 Write-Host "Applying Latest Windows Updates"
-update-mywindowsimage -path c: -update all -
+update-mywindowsimage -path c: -update all
 
 # Drop a custom unattend.xml which runs a post-install script
 #New-Item c:\Windows\system32\Linklaters\OOBE -force -ItemType Directory
