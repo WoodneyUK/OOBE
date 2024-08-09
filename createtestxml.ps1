d1811e9a2dd647c383695df7af67387cd42396be
WoodneyUK <55578109+WoodneyUK@users.noreply.github.com>
2024-08-09 09:58:50 +0100

    Create createtestxml.ps1
    Signed-off-by: WoodneyUK <55578109+WoodneyUK@users.noreply.github.com>

---
+++ createtestxml.ps1
@@ -0,0 +1,19 @@
+$XMLPath = "c:\windows\panther\unattend\unattend.xml"
+
+$UnattendXml = [xml] @'
+<?xml version="1.0" encoding="utf-8"?>
+<unattend xmlns="urn:schemas-microsoft-com:unattend">
+    <settings pass="auditSystem">
+        <component name="Microsoft-Windows-Deployment" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+            <Reseal>
+                <Mode>Audit</Mode>
+            </Reseal>
+        </component>
+    </settings>
+    <cpi:offlineImage cpi:source="wim:c:/win11-unattend/sources/install.wim#Windows 11 Enterprise" xmlns:cpi="urn:schemas-microsoft-com:cpi" />
+</unattend>
+'@
+
+md c:\windows\panther\unattend -Force
+
+$UnattendXml.Save($XMLPath)
