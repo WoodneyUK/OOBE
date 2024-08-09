7fbabdad89ef214770798c1326ecda7cdcecbd38
WoodneyUK <55578109+WoodneyUK@users.noreply.github.com>
2024-08-09 11:44:35 +0100

    Update createtestxml.ps1
    Signed-off-by: WoodneyUK <55578109+WoodneyUK@users.noreply.github.com>

--- createtestxml.ps1
+++ createtestxml.ps1
@@ -9,36 +9,42 @@ $UnattendXml = [xml] @'
                 <Mode>Audit</Mode>
             </Reseal>
         </component>
+        <component name="Microsoft-Windows-International-Core" processorArchitecture="wow64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+            <SystemLocale>en-US</SystemLocale>
+        </component>
     </settings>
     <settings pass="auditUser">
         <component name="Microsoft-Windows-Deployment" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
             <RunSynchronous>
                 <RunSynchronousCommand wcm:action="add">
-                <Order>1</Order>
-                <Description>Set ExecutionPolicy Bypass</Description>
-                <Path>PowerShell -WindowStyle Hidden -Command "Set-ExecutionPolicy Bypass -Force"</Path>
+                    <Order>1</Order>
+                    <Description>Set ExecutionPolicy Bypass</Description>
+                    <Path>PowerShell -WindowStyle Hidden -Command "Set-ExecutionPolicy Bypass -Force"</Path>
                 </RunSynchronousCommand>
-
                 <RunSynchronousCommand wcm:action="add">
-                <Order>2</Order>
-                <Description>WaitWebConnection</Description>
-                <Path>PowerShell -Command "Wait-WebConnection powershellgallery.com -Verbose"</Path>
+                    <Order>2</Order>
+                    <Description>WaitWebConnection</Description>
+                    <Path>PowerShell -Command "Wait-WebConnection powershellgallery.com -Verbose"</Path>
                 </RunSynchronousCommand>
-
                 <RunSynchronousCommand wcm:action="add">
-                <Order>3</Order>
-                <Description>Save Get-WindowsAutoPilotInfo</Description>
-                <Path>PowerShell -Command "Install-Script -Name Get-WindowsAutoPilotInfo -Verbose -Force"</Path>
+                    <Order>3</Order>
+                    <Description>Save Get-WindowsAutoPilotInfo</Description>
+                    <Path>PowerShell -Command "Install-Script -Name Get-WindowsAutoPilotInfo -Verbose -Force"</Path>
                 </RunSynchronousCommand>
-                
                 <RunSynchronousCommand wcm:action="add">
-                <Order>4</Order>
-                <Description>Installing Windows Updates</Description>
-                <Path>PowerShell -Command "start-windowsupdate"</Path>
+                    <Order>4</Order>
+                    <Description>Installing Windows Updates</Description>
+                    <Path>PowerShell -Command "start-windowsupdate"</Path>
                 </RunSynchronousCommand>
             </RunSynchronous>
         </component>
     </settings>
+    <settings pass="specialize">
+        <component name="Microsoft-Windows-International-Core" processorArchitecture="wow64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+            <SystemLocale>en-US</SystemLocale>
+        </component>
+    </settings>
+    <cpi:offlineImage cpi:source="wim:c:/win11-unattend/sources/install.wim#Windows 11 Enterprise" xmlns:cpi="urn:schemas-microsoft-com:cpi" />
 </unattend>
 '@
 
