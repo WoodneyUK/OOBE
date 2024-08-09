ee5ea14951421793d9643d1fb5f6ca972ba983de
WoodneyUK <55578109+WoodneyUK@users.noreply.github.com>
2024-08-09 10:26:35 +0100

    Update createtestxml.ps1
    Signed-off-by: WoodneyUK <55578109+WoodneyUK@users.noreply.github.com>

--- createtestxml.ps1
+++ createtestxml.ps1
@@ -3,14 +3,36 @@ $XMLPath = "c:\windows\panther\unattend\unattend.xml"
 $UnattendXml = [xml] @'
 <?xml version="1.0" encoding="utf-8"?>
 <unattend xmlns="urn:schemas-microsoft-com:unattend">
-    <settings pass="auditSystem">
+    <settings pass="oobeSystem">
         <component name="Microsoft-Windows-Deployment" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
             <Reseal>
                 <Mode>Audit</Mode>
             </Reseal>
         </component>
     </settings>
-    <cpi:offlineImage cpi:source="wim:c:/win11-unattend/sources/install.wim#Windows 11 Enterprise" xmlns:cpi="urn:schemas-microsoft-com:cpi" />
+    <settings pass="auditUser">
+        <component name="Microsoft-Windows-Deployment" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+            <RunSynchronous>
+                <RunSynchronousCommand wcm:action="add">
+                <Order>1</Order>
+                <Description>Set ExecutionPolicy Bypass</Description>
+                <Path>PowerShell -WindowStyle Hidden -Command "Set-ExecutionPolicy Bypass -Force"</Path>
+                </RunSynchronousCommand>
+
+                <RunSynchronousCommand wcm:action="add">
+                <Order>2</Order>
+                <Description>WaitWebConnection</Description>
+                <Path>PowerShell -Command "Wait-WebConnection powershellgallery.com -Verbose"</Path>
+                </RunSynchronousCommand>
+
+                <RunSynchronousCommand wcm:action="add">
+                <Order>3</Order>
+                <Description>Save Get-WindowsAutoPilotInfo</Description>
+                <Path>PowerShell -Command "Install-Script -Name Get-WindowsAutoPilotInfo -Verbose -Force"</Path>
+                </RunSynchronousCommand>
+            </RunSynchronous>
+        </component>
+    </settings>
 </unattend>
 '@
 
