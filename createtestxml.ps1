8c00592c7f2c5c3c87c004e9909da81c44fb958b
WoodneyUK <55578109+WoodneyUK@users.noreply.github.com>
2024-08-26 19:29:10 +0100

    Update createtestxml.ps1

--- createtestxml.ps1
+++ createtestxml.ps1
@@ -89,12 +89,12 @@ $boottowindows = [xml] @'
                 <RunSynchronousCommand wcm:action="add">
                     <Order>8</Order>
                     <Description>LL:Rename Unattend.xml</Description>
-                    <Path>rename c:\windows\panther\unattend\unattend.xml c:\windows\panther\unattend\unattend.old</Path>
+                    <Path>rename c:\windows\panther\unattend\unattend.xml unattend.old</Path>
                 </RunSynchronousCommand>
                 <RunSynchronousCommand wcm:action="add">
                     <Order>9</Order>
                     <Description>LL:Rename OOBE.xml</Description>
-                    <Path>rename c:\windows\panther\unattend\unattend\oobe.xml c:\windows\panther\unattend\unattend.xml</Path>
+                    <Path>rename c:\windows\panther\unattend\oobe.xml unattend.xml</Path>
                 </RunSynchronousCommand>
                 <RunSynchronousCommand wcm:action="add">
                     <Order>10</Order>
