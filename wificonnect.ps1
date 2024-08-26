d92abb01ec36995b18c7e5f0de6464a1533ea786
WoodneyUK <55578109+WoodneyUK@users.noreply.github.com>
2024-08-26 15:21:46 +0100

    Create wificonnect.ps1
    Signed-off-by: WoodneyUK <55578109+WoodneyUK@users.noreply.github.com>

---
+++ wificonnect.ps1
@@ -0,0 +1,11 @@
+$connection = Get-Content "C:\OSDCloud\configs\WiFi.JSON" -raw | Convertfrom-json
+
+Get-ChildItem c:\osdcloud\configs\*.xml | foreach {
+    netsh wlan add profile filename="$_."
+    [xml] $import = Get-Content $_
+    If ($connection.Addons.SSID -eq $import.wlanprofile.ssidconfig.ssid.name){
+        netsh wlan connect name=$($import.wlanprofile.name) SSID=$($import.wlanprofile.ssidconfig.ssid.name)
+    }
+}
+
+start-sleep 30
