0994a69cd579bf479c70602f41ba2e3151ee6e70
WoodneyUK <55578109+WoodneyUK@users.noreply.github.com>
2024-08-26 20:34:39 +0100

    Update StartAuditMode.ps1

--- StartAuditMode.ps1
+++ StartAuditMode.ps1
@@ -11,7 +11,7 @@ write-host "This is a custom Audit Mode script"
 # Shell out to reboot
 #Start-Process -FilePath "cmd.exe" -ArgumentList '/c "timeout /t 3 /nobreak && shutdown -r -f -t 0"' -WindowStyle Hidden
 
-remove-item c:\windows\panther\unattend\unattend.xml
+rename-item -path c:\windows\panther\unattend\unattend.xml -newname unattend.old
 rename-item -path c:\windows\panther\unattend\oobe.xml -newname unattend.xml
 
 c:\windows\system32\sysprep\sysprep.exe /quiet /reboot /oobe /unattend:c:\windows\panther\unattend\unattend.xml
