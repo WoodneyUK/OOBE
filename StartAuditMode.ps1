0051e0abcec43a9d7374e03a9a042c288f7635dc
WoodneyUK <55578109+WoodneyUK@users.noreply.github.com>
2024-08-26 20:21:54 +0100

    Update StartAuditMode.ps1

--- StartAuditMode.ps1
+++ StartAuditMode.ps1
@@ -3,7 +3,7 @@ write-host "This is a custom Audit Mode script"
 #Write-Host "Setting Reg"
 #Set-ItemProperty -Path HKLM:\SYSTEM\Setup\Status -Name AuditBoot -Value 0
 
-start-sleep 30
+#start-sleep 30
 
 
 
@@ -11,11 +11,13 @@ start-sleep 30
 # Shell out to reboot
 #Start-Process -FilePath "cmd.exe" -ArgumentList '/c "timeout /t 3 /nobreak && shutdown -r -f -t 0"' -WindowStyle Hidden
 
-# set exit code 1 so that Windows knows it needs a reboot
-# https://learn.microsoft.com/en-us/windows-hardware/customize/desktop/unattend/microsoft-windows-deployment-runsynchronous-runsynchronouscommand-willreboot#values
-#Exit 1
+remove-item c:\windows\panther\unattend\unattend.xml
+rename-item -path c:\windows\panther\unattend\oobe.xml -newname unattend.xml
 
-
+c:\windows\system32\sysprep\sysprep.exe /quiet /reboot /oobe /unattend:c:\windows\panther\unattend\unattend.xml
 
 #without reboot
-exit 0
+#exit 0
+
+#with reboot
+exit 1
