f76cb92bd9fe65b2ec8043b6612f9bc0fd028157
WoodneyUK <55578109+WoodneyUK@users.noreply.github.com>
2024-08-26 18:00:49 +0100

    Update StartAuditMode.ps1
    Signed-off-by: WoodneyUK <55578109+WoodneyUK@users.noreply.github.com>

--- StartAuditMode.ps1
+++ StartAuditMode.ps1
@@ -1,9 +1,13 @@
 write-host "This is a custom Audit Mode script"
 
-Write-Host "Setting Reg"
+#Write-Host "Setting Reg"
 #Set-ItemProperty -Path HKLM:\SYSTEM\Setup\Status -Name AuditBoot -Value 0
 
 start-sleep 30
 
-Write-Host "restarting"
-restart-computer -force
+# Shell out to reboot
+Start-Process -FilePath "cmd.exe" -ArgumentList '/c "timeout /t 3 /nobreak && shutdown -r -f -t 0"' -WindowStyle Hidden
+
+# set exit code 1 so that Windows knows it needs a reboot
+# https://learn.microsoft.com/en-us/windows-hardware/customize/desktop/unattend/microsoft-windows-deployment-runsynchronous-runsynchronouscommand-willreboot#values
+Exit 1
