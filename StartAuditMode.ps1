d16c0a75e284b008478308e22ca8ff670f77f8eb
WoodneyUK <55578109+WoodneyUK@users.noreply.github.com>
2024-08-26 16:01:03 +0100

    Update StartAuditMode.ps1
    Signed-off-by: WoodneyUK <55578109+WoodneyUK@users.noreply.github.com>

--- StartAuditMode.ps1
+++ StartAuditMode.ps1
@@ -1,9 +1,9 @@
 write-host "This is a custom Audit Mode script"
 
 Write-Host "Setting Reg"
-Set-ItemProperty -Path HKLM:\SYSTEM\Setup\Status -Name AuditBoot -Value 0
+#Set-ItemProperty -Path HKLM:\SYSTEM\Setup\Status -Name AuditBoot -Value 0
 
-start-sleep 5
+start-sleep 30
 
 Write-Host "restarting"
 restart-computer -force
