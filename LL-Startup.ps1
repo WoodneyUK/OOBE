db6e41fea57e1cce3222b22d007bba0557bb68b9
WoodneyUK <55578109+WoodneyUK@users.noreply.github.com>
2024-08-28 09:23:56 +0100

    Update LL-Startup.ps1
    Signed-off-by: WoodneyUK <55578109+WoodneyUK@users.noreply.github.com>

--- LL-Startup.ps1
+++ LL-Startup.ps1
@@ -23,9 +23,9 @@ Else { write-host "Supported Device check complete" }
 
 If ($manufacturer -eq "Lenovo"){
     Write-Host "Running BIOS Script"
-    $BIOSScript = Invoke-RestMethod https://raw.githubusercontent.com/WoodneyUK/OOBE/main/Lenovo_BIOS_Settings.ps1
-    $BIOSScriptBlock = [scriptblock]::Create($BIOSScript.Content)
-    Invoke-Command -ScriptBlock $BIOSScriptBlock
+    new-item X:\Temp -itemtype Directory
+    $BIOSScript = Invoke-RestMethod https://raw.githubusercontent.com/WoodneyUK/OOBE/main/Lenovo_BIOS_Settings.ps1 | out-file "X:\Temp\Lenovo_BIOS_Settings.ps1" -force -encoding ascii
+    & X:\temp\Lenovo_BIOS_Settings.ps1
     }
 
 ## Lenovo Keyboard layout
