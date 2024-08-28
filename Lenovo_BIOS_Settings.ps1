0f8fcb20889d1d521314d1b7ef52c302f1433f42
WoodneyUK <55578109+WoodneyUK@users.noreply.github.com>
2024-08-28 09:18:13 +0100

    Update Lenovo_BIOS_Settings.ps1
    Signed-off-by: WoodneyUK <55578109+WoodneyUK@users.noreply.github.com>

--- Lenovo_BIOS_Settings.ps1
+++ Lenovo_BIOS_Settings.ps1
@@ -1,57 +1,10 @@
-<#
-.SYNOPSIS
-Get BIOS Settings for Lenovo devices, based on custom definitions in script
-
-.EXAMPLE
-.\Lenovo_BIOS_Settings_Remediate.ps1
-
-.DESCRIPTION
-Remediate custom BIOS settings for Lenovo devices
-
-.NOTES
-   Version:		0.1
-   Creation Date:	12-10-2022
-   Author:		Ákos Bakos
-   Company:		SmartCon GmbH
-   Contact:		akos.bakos@smartcon.ch
-
-   Copyright (c) 2022 SmartCon GmbH
-
-HISTORY:
-Date			By			Comments
-----------		---			----------------------------------------------------------
-
-#>
-
-$Global:Transcript = "$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-Set_Lenovo_BIOS_Settings.log"
-Start-Transcript -Path (Join-Path "C:\OSDCloud\Logs\" $Global:Transcript) -ErrorAction Ignore
-
-If(!(Test-Path $Log_File)){New-Item $Log_File -Type file -Force}
-Function Write-Log
-	{
-	param(
-	$Message_Type, 
-	$Message
-	)
-		$MyDate = "[{0:MM/dd/yy} {0:HH:mm:ss}]" -f (Get-Date)
-		Add-Content $Log_File  "$MyDate - $Message_Type : $Message"
-		Write-Host  "$MyDate - $Message_Type : $Message"
-	} 
-
-	# Get manufacturer 
 $Get_Manufacturer_Info = (Get-WmiObject win32_computersystem).Manufacturer
 If($Get_Manufacturer_Info -notlike "*lenovo*")	
 	{
-		Write-Log -Message_Type "ERROR" -Message "Device manufacturer not supported"											
 		Write-Output "Device manufacturer not supported"
 		EXIT 1			
 	}
-Else	
-	{
-		Write-Log -Message_Type "INFO" -Message "Device manufacturer is Lenovo"											
-	}
 
-Write-Log -Message_Type "INFO" -Message "Staring the script"
 
 # Define custom settings
 $Get_Settings = @(
@@ -101,17 +54,15 @@ ForEach($Settings in $Get_Settings)
     {
         $MySetting = $Settings.Setting
         $NewValue = $Settings.Value				
-        Write-Log -Message_Type "INFO" -Message "Change to do: $MySetting - $NewValue"  
         $Change_Return_Code = $BIOS.SetBiosSetting("$MySetting,$NewValue").Return
 
         If(($Change_Return_Code) -eq "Success")        								
             {
-                Write-Log -Message_Type "INFO" -Message "New value for $MySetting is $NewValue"  	
-                Write-Log -Message_Type "SUCCESS" -Message "The setting has been set"  												
+                Write-Host "New value for $($MySetting) is $($NewValue)"  											
             }
         Else
             {
-                Write-Log -Message_Type "ERROR" -Message "Can not change setting $MySetting (Return code $Change_Return_Code)"  											
+                Write-Warning "Cannot change setting $($MySetting) (Return code $($Change_Return_Code))"  											
             }								
     }
 
@@ -120,11 +71,9 @@ $Save_BIOS = (Get-WmiObject -class Lenovo_SaveBiosSettings -namespace root\wmi)
 $Save_Change_Return_Code = $SAVE_BIOS.SaveBiosSettings().Return		
 If(($Save_Change_Return_Code) -eq "Success")
 	{
-		Write-Log -Message_Type "SUCCESS" -Message "BIOS settings have been saved"
-		Add-Content $Log_File ""																
+		Write-Host "BIOS settings have been saved"																
 	}
 Else
 	{
-		Write-Log -Message_Type "ERROR" -Message "An issue occured while saving changes - $Save_Change_Return_Code"
-		Add-Content $Log_File ""																		
+		Write-Warning "An issue occured while saving changes - $($Save_Change_Return_Code)"										
 	}
