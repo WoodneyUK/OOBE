8243a73c1262f16717771928f8ad0dd9ea650d6e
WoodneyUK <55578109+WoodneyUK@users.noreply.github.com>
2024-08-29 16:25:38 +0100

    Update LL-Startup.ps1
    Signed-off-by: WoodneyUK <55578109+WoodneyUK@users.noreply.github.com>

--- LL-Startup.ps1
+++ LL-Startup.ps1
@@ -213,7 +213,7 @@ $OOBEDeployJson = @'
 #Invoke-restmethod https://raw.githubusercontent.com/WoodneyUK/OOBE/main/OOBEDeploy.ps1 | out-file "c:\windows\setup\scripts\oobe.ps1" -force -encoding ascii
 #Invoke-restmethod https://raw.githubusercontent.com/WoodneyUK/OOBE/main/oobe.cmd | out-file "c:\windows\setup\scripts\oobe.cmd" -force -encoding ascii
 Invoke-restmethod https://raw.githubusercontent.com/WoodneyUK/OOBE/main/createtestxml.ps1 | out-file "c:\windows\setup\scripts\createtestxml.ps1" -force -encoding ascii
-#invoke-restmethod https://raw.githubusercontent.com/WoodneyUK/OOBE/main/wificonnect.ps1 | out-file "c:\windows\setup\scripts\wificonnect.ps1" -force -encoding ascii
+invoke-restmethod https://raw.githubusercontent.com/WoodneyUK/OOBE/main/wificonnect.ps1 | out-file "c:\windows\setup\scripts\wificonnect.ps1" -force -encoding ascii
 
 #Custom unattend.xml
 New-Item c:\windows\panther\unattend -force -ItemType Directory
