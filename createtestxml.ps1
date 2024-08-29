ac80b6dabd7836ce3cc727a98d2385f9ff8f5c8d
WoodneyUK <55578109+WoodneyUK@users.noreply.github.com>
2024-08-29 15:57:50 +0100

    Update createtestxml.ps1
    Signed-off-by: WoodneyUK <55578109+WoodneyUK@users.noreply.github.com>

--- createtestxml.ps1
+++ createtestxml.ps1
@@ -42,14 +42,6 @@ $boottowindows = [xml] @'
                 <Mode>Audit</Mode>
             </Reseal>
         </component>
-        <component name="Microsoft-Windows-Shell-Setup" processorArchitecture="wow64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-            <OOBE>
-                <HideEULAPage>true</HideEULAPage>
-                <HideLocalAccountScreen>true</HideLocalAccountScreen>
-                <HideOEMRegistrationScreen>true</HideOEMRegistrationScreen>
-                <ProtectYourPC>3</ProtectYourPC>
-            </OOBE>
-        </component>
     </settings>
     <settings pass="auditUser">
         <component name="Microsoft-Windows-Deployment" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
