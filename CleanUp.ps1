ef731c721694fa5b95200b7d648a7f0650c99f9e
WoodneyUK <55578109+WoodneyUK@users.noreply.github.com>
2024-08-29 10:23:22 +0100

    Update CleanUp.ps1
    Signed-off-by: WoodneyUK <55578109+WoodneyUK@users.noreply.github.com>

--- CleanUp.ps1
+++ CleanUp.ps1
@@ -1,3 +1,10 @@
 # Script goes here to cleanup the disk after OS installation
 
+try { stop-transcript }
+catch { Write-host "Transcript not running"}
+
+new-item c:\windows\system32\linklaters\logs -itemtype directory -force
+
+copy-item -path c:\osdcloud\logs\*.* -destination c:\windows\system32\linklaters\logs -recurse
+
 remove-item c:\osdcloud -force -recurse
