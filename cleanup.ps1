d62acf12b08d4c4de28173b56743561bd7f3074f
WoodneyUK <55578109+WoodneyUK@users.noreply.github.com>
2024-09-06 12:12:39 +0100

    Create cleanup.ps1
    Signed-off-by: WoodneyUK <55578109+WoodneyUK@users.noreply.github.com>

---
+++ cleanup.ps1
@@ -0,0 +1,11 @@
+##  Script runs in full Windows11 Audit mode
+
+try { stop-transcript }
+catch { Write-host "Transcript not running"}
+
+new-item c:\windows\system32\linklaters\logs -itemtype directory -force
+
+copy-item -path c:\osdcloud\logs\*.* -destination c:\windows\system32\linklaters\logs -recurse
+
+Get-ChildItem -Path c:\osdcloud -Recurse | Remove-Item -force -recurse
+remove-item c:\osdcloud -force
