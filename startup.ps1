79e3b98bf67d18e2435bf1d259b8d3b7ad1d091f
WoodneyUK <55578109+WoodneyUK@users.noreply.github.com>
2024-09-06 14:06:14 +0100

    Update startup.ps1
    Signed-off-by: WoodneyUK <55578109+WoodneyUK@users.noreply.github.com>

--- startup.ps1
+++ startup.ps1
@@ -153,6 +153,7 @@ reg unload "HKLM\NewOS"
 # Download custom file(s)
 Invoke-restmethod -uri "$($Global:ScriptRootURL)/createxml.ps1" | out-file "c:\windows\setup\scripts\createxml.ps1" -force -encoding ascii
 invoke-restmethod -uri "$($Global:ScriptRootURL)/wificonnect.ps1" | out-file "c:\windows\setup\scripts\wificonnect.ps1" -force -encoding ascii
+Invoke-restmethod -uri "$($Global:ScriptRootURL)/CleanUp.ps1" | out-file "c:\windows\setup\scripts\LLCleanup.ps1" -force -encoding ascii
 
 #Custom unattend.xml
 New-Item c:\windows\panther\unattend -force -ItemType Directory
