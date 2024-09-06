be607d7be0de563934ea1978d5efd5c1f3593a0e
WoodneyUK <55578109+WoodneyUK@users.noreply.github.com>
2024-09-06 14:07:03 +0100

    Update cleanup.ps1
    Signed-off-by: WoodneyUK <55578109+WoodneyUK@users.noreply.github.com>

--- cleanup.ps1
+++ cleanup.ps1
@@ -5,7 +5,7 @@ catch { Write-host "Transcript not running"}
 
 new-item c:\windows\system32\linklaters\logs -itemtype directory -force
 
-copy-item -path c:\osdcloud\logs\*.* -destination c:\windows\system32\linklaters\logs -recurse
+copy-item -path c:\osdcloud\logs\*.* -destination c:\windows\system32\linklaters\logfiles -recurse
 
 Get-ChildItem -Path c:\osdcloud -Recurse | Remove-Item -force -recurse
 remove-item c:\osdcloud -force
