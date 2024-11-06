14c0db3836f212faac40b8c1d2edae6c1fe08c66
WoodneyUK <55578109+WoodneyUK@users.noreply.github.com>
2024-11-06 17:33:08 +0000

    Update startup.ps1
    Signed-off-by: WoodneyUK <55578109+WoodneyUK@users.noreply.github.com>

--- startup.ps1
+++ startup.ps1
@@ -180,7 +180,6 @@ $Global:MyOSDCloud = [ordered]@{
     SyncMSUpCatDriverUSB = [bool]$false
     ApplyCatalogFirmware = [bool]$true
     ApplyCatalogDrivers = [bool]$false
-    Logs = [string]"C:\Windows\System32\Linklaters\Logfiles"
     CheckSHA1 = [bool]$false
     OSImageIndex = [int32]3
     ImageFileFullName = [string]"$($USBDataVol.driveletter):\OSDCloud\os\install.wim"
