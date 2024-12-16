afdb9b9c07c35d1d042bf65797492bf3f241d1e0
WoodneyUK <55578109+WoodneyUK@users.noreply.github.com>
2024-12-16 15:10:46 +0000

    increment to 2.0
    Signed-off-by: WoodneyUK <55578109+WoodneyUK@users.noreply.github.com>

--- startup.ps1
+++ startup.ps1
@@ -8,7 +8,7 @@
 $DateFormat = 'dd-MM-yyyy HH:mm:ss'
 $OSDCloud_StartTimeUTC = $(Get-Date ([System.DateTime]::UtcNow) -Format $DateFormat)
 ## Run from URL
-Write-host "LL startup v1.9.2"
+Write-host "LL startup v2.0"
 Write-Host "Running from [$($Global:ScriptRootURL)]"
 start-sleep 5
 
